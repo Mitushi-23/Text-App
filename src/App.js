9de22d8d9eeee797569a400a35e2327516555de3
Mitushi-23 <mitushi2agrawal@gmail.com>
2021-09-07 02:52:22 +0530

    enabled dark mode

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+
+import React, { useState } from 'react'
+
 function App() {
+  const [mode, setmode] = useState('light');
+const toggleMode = () =>{
+  if(mode === 'light')
+  {
+    setmode('dark');
+    document.body.style.backgroundColor = '#242326db';
+  }
+  else{
+    setmode('light');
+    document.body.style.backgroundColor = 'white';
+
+  }
+}
+
+  
   return (
     <>
-      <Navbar title="Portfolio" />
-      <TextForm heading="Enter the text below" />
+      <Navbar title="Portfolio" mode={mode} toggleMode={toggleMode}/>
+      <TextForm heading="Enter the text below" mode = {mode} />
     </>
   );
 }
